4d61841c59726633b11eb98d7fa260939b28d181
Yariki <iyariki.ya@gmail.com>
2018-10-10 10:26:05 +0300

    added UI for deleting record

--- funtask.js
+++ funtask.js
@@ -20,7 +20,23 @@ var SortingType = {
     desc: 1
 };
 
-var Project = function (projectName, dueDate, created, members, type, status, customer) {
+var IndexRepository = (function(){
+    var index = 0;
+
+    var getNextIndex = function () {
+        return ++i;
+    };
+
+    return {
+        getNextIndex: function(){
+            return getNextIndex();
+        }
+    }
+})();
+
+
+var Project = function (id, projectName, dueDate, created, members, type, status, customer) {
+    this.Id = id;
     this.ProjectName = projectName;
     this.DueDate = dueDate;
     this.Created = created;
@@ -31,11 +47,11 @@ var Project = function (projectName, dueDate, created, members, type, status, cu
 };
 
 var data = [
-    new Project('PCo', new Date(2019,12,2), new Date(2005,12,6),null,Type.customers, Status.none, 'SAP'),
-    new Project('WebSped', new Date(2017,12,15), new Date(2017,12,15),null,Type.customers, Status.completed, 'LIS'),
-    new Project('Outlookfinder', new Date(2017,10,12), new Date(2010,8,1),null,Type.customers, Status.inProgress, 'Vincent Payette'),
-    new Project('Windows', new Date(1989,10,12), new Date(2020,12,31),null,Type.customers, Status.new, 'Microsoft'),
-    new Project('Linux', new Date(1991,10,12), new Date(2020,12,31),null,Type.company, Status.inProgress, 'Torvalds')
+    new Project(IndexRepository.getNextIndex(), 'PCo', new Date(2019,12,2), new Date(2005,12,6),null,Type.customers, Status.none, 'SAP'),
+    new Project(IndexRepository.getNextIndex(),'WebSped', new Date(2017,12,15), new Date(2017,12,15),null,Type.customers, Status.completed, 'LIS'),
+    new Project(IndexRepository.getNextIndex(),'Outlookfinder', new Date(2017,10,12), new Date(2010,8,1),null,Type.customers, Status.inProgress, 'Vincent Payette'),
+    new Project(IndexRepository.getNextIndex(),'Windows', new Date(1989,10,12), new Date(2020,12,31),null,Type.customers, Status.new, 'Microsoft'),
+    new Project(IndexRepository.getNextIndex(),'Linux', new Date(1991,10,12), new Date(2020,12,31),null,Type.company, Status.inProgress, 'Torvalds')
 ];
 
 var DomStrings = {
@@ -43,6 +59,8 @@ var DomStrings = {
     table: "table",
     dataColumnSortAttribute: 'data-column-sort',
     dataColumnPropertyAttribute: 'data-property',
+    dataColumnDeleteAttrubute: 'data-delete-record',
+    dataColumnIdAttribute: 'data-id'
 };
 
 function handleMenu(){
@@ -107,23 +125,53 @@ var tableController = (function(){
         setData(data);
     };
 
-    var setData = function(dataSet) {
-        var body = document.getElementById(DomStrings.tableBody);
+     var removeTableData = function(body) {
+         processDeleteSubscribtion(body,true);
         var child;
         while (child = body.firstChild)
             body.removeChild(child);
+    };
+
+    var onDeleteItem = function (event) {
+        var id = +this.getAttribute(DomStrings.dataColumnIdAttribute);
+        var item = data.find(value => value.Id == id);
+        var index = data.indexOf(item);
+        data.splice(index,1);
+
+    }
+
+    var processDeleteSubscribtion = function (body, isDeleteSubscription) {
+        var links = body.querySelectorAll('['+ DomStrings.dataColumnDeleteAttrubute +']');
+        for(var i = 0; i < links.length; i++){
+            var link = links[i];
+            if(isDeleteSubscription){
+                link.removeEventListener('click',onDeleteItem);
+            }else{
+                link.addEventListener('click',onDeleteItem);
+            }
+        }
+    };
+
+
+    var setData = function(dataSet) {
+        var body = document.getElementById(DomStrings.tableBody);
+
+        removeTableData(body);
 
         for (var d in dataSet) {
             var tr = createTag('tr',{},body);
-            var td = createTag('td',{class:'firstCol'},tr);
-            td.innerText = dataSet[d].ProjectName;
+            tr.setAttribute(DomStrings.dataColumnIdAttribute, dataSet[d].Id);
+            createTag('td',{class:'firstCol'},tr).innerText = dataSet[d].ProjectName;
             createTag('td',{},tr).innerText = dataSet[d].DueDate.toDateString();
             createTag('td',{},tr).innerText = dataSet[d].Created.toDateString();
             createTag('td',{},tr).innerText = dataSet[d].Members;
             createTag('td',{},tr).innerText = getType(dataSet[d].Type);
             createTag('td',{},tr).innerText = getStatus(dataSet[d].Status);
             createTag('td',{},tr).innerText = dataSet[d].Customer;
+            createTag('td',{},tr).innerHTML = '<a href="#" data-delete-record data-id="'+ dataSet[d].Id +'"><i class="fa fa-trash"></i></a>';
         }
+
+        processDeleteSubscribtion(body,false);
     };
 
     var subscribeHeader = function(){
